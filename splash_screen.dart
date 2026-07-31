c90de869118d0eddf4946c9b978b2b682d2906b8
alfiya10208-alt <alfiya10208@gmail.com>
2026-07-31 17:52:42 +0530

    Create splash_screen.dart
    splash screen

---
+++ splash_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:internship/login_page.dart';
+import 'dart:async';
+import 'home_page.dart'; 
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Background Image
+          Image.asset(
+            "assets/splash_bg.jpg",
+            fit: BoxFit.cover,
+          ),
+
+          // Dark Overlay
+          Container(
+            color: Colors.black.withOpacity(0.6),
+          ),
+
+          // App Logo & Name
+          AnimatedOpacity(
+            opacity: 1.0,
+            duration: Duration(seconds: 2),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.travel_explore, color: Colors.white, size: 80),
+                SizedBox(height: 20),
+                Text(
+                  "MahaExplore",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
