c9e69dfecf99d0ce1c5757194fd754638306faf6
alfiya10208-alt <alfiya10208@gmail.com>
2026-08-29 16:17:19 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:internship/splash_screen.dart';
 import 'firebase_options.dart';
-import 'login_page.dart';
+import 'splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,15 +10,18 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      title: 'Explore Maharashtra',
+      home: SplashScreen(),
     );
   }
 }
